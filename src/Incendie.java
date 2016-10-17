a4bfecdf8505961e1444a4877a8686b2a2bbc051
thomas Pouget-abadie <toefjerf@gmail.com>
2016-10-17 08:43:10 +0200

    creation of Incendie file

---
+++ src/Incendie.java
@@ -0,0 +1,19 @@
+/**
+ * Incendie:
+ * Contient une case et une quantiteEau qui correspond à la
+ * quantité d'eau nécessaire pour éteindre l'incendie
+ * @author pougetat
+ */
+
+
+public class Incendie {
+    
+    Case position;
+    int quantiteEau;
+    
+    public Incendie(Case positon, int quantiteEau) {
+	this.position = position;
+	this.quantiteEau = quantiteEau;
+    }
+
+}
